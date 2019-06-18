a292e27e4a986630480523d7356534a9ee1735e4
Cash Costello <cash.costello@jhuapl.edu>
2019-06-18 09:35:52 -0400

    template strings are not supported by IE

--- dragonfly/static/js/utilities.js
+++ dragonfly/static/js/utilities.js
@@ -95,7 +95,7 @@ function copyToClipboard(text) {
         fakeElem.style[ isRTL ? 'right' : 'left' ] = '-9999px';
         // Move element to the same position vertically
         let yPosition = window.pageYOffset || document.documentElement.scrollTop;
-        fakeElem.style.top = `${yPosition}px`;
+        fakeElem.style.top = yPosition + 'px';
 
         fakeElem.setAttribute('readonly', '');
         fakeElem.value = text;
